453cbb10ff096bf194919a78c4aab1d1dc20b2bf
Marta Waplak <mwaplak@gmail.com>
2018-06-04 21:21:35 +0200

    Fix problem with storing lane name in database

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,9 +3,9 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
-router.route('/lanes').post(LaneController.addLane);
 router.route('/lanes').get(LaneController.getLanes);
+router.route('/lanes').post(LaneController.addLane);
 router.route('/lanes/:laneId').delete(LaneController.deleteLane);
-router.route('/lanes/:laneId').put(LaneController.updateLane);
+router.route('/lanes').put(LaneController.updateLane);
 
 export default router;
